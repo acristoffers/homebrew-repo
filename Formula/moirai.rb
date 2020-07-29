9e806009e8d258dd93c1f7ec1e618856ca5923d4
Álan Crístoffer <acristoffers@gmail.com>
2020-07-29 18:18:43 -0300

    Version 1.3.18

--- Formula/moirai.rb
+++ Formula/moirai.rb
@@ -1,22 +1,23 @@
 class Moirai < Formula
-  desc "Control platform server (Lachesis backend)"
-  homepage "https://github.com/acristoffers/moirai"
-  url "https://files.pythonhosted.org/packages/17/58/7a4f045688811258c10815fc5331c45f4c2adce8ec6f812c3627c8446a2d/moirai-1.3.17.tar.gz"
-  version "1.3.17"
-  sha256 "921bf9591c15d5c824bcde0967f2fcbe342108d67b147ff2f6519e11ab03d900"
-  license "mit"
+  desc 'Control platform server (Lachesis backend)'
+  homepage 'https://github.com/acristoffers/moirai'
+  url 'https://files.pythonhosted.org/packages/a9/f1/e7fdd3c86beb58043383ee43479492dda53253a7bb088f73a5abc7f36633/moirai-1.3.18.tar.gz'
+  version '1.3.18'
+  sha256 'd1463ef74555c1c7c9429b6409927e3bc21ecafd45c3546c050b8c22941e9246'
+  license 'mit'
 
-  depends_on "python@3" => :build
-  depends_on "mongodb/brew/mongodb-community"
-  depends_on "python@3"
-  depends_on "snap7"
+  depends_on 'python@3' => :build
+  depends_on 'mongodb/brew/mongodb-community'
+  depends_on 'python@3'
+  depends_on 'snap7'
 
   def install
-    system Formula["python@3.8"].opt_bin/"python3", *Language::Python.setup_install_args(prefix)
-    system Formula["python@3.8"].opt_bin/"python3", "-m", "moirai", "--install"
+    python3 = Formula['python@3.8'].opt_bin / 'python3'
+    system python3, *Language::Python.setup_install_args(prefix)
+    system python3, '-m', 'moirai', '--install'
   end
 
   test do
-    system "moirai", "--version"
+    system 'moirai', '--version'
   end
 end
