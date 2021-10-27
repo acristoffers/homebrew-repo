4bd64718137e11c6494196fc5405b42054dc843f
Álan Crístoffer <acristoffers@gmail.com>
2021-10-27 10:45:23 +0200

    Version 1.3.5

--- Casks/lachesis.rb
+++ Casks/lachesis.rb
@@ -1,19 +1,25 @@
 cask 'lachesis' do
-  version '1.3.4'
-  sha256 'd93ad4e33d0050a5cf006a35dae9c5c0a5fc6b950a54e7061ed9da7ba07037ee'
+  version '1.3.5'
 
-  url "https://github.com/acristoffers/Lachesis/releases/download/v#{version}/Lachesis-#{version}.dmg"
+  if Hardware::CPU.intel?
+    sha256 '9d8dc153d39b4a2fa5643688b20c3be5eb8308e855b80c34a7a7603fdd0a9e42'
+    url "https://github.com/acristoffers/Lachesis/releases/download/v#{version}/Lachesis-#{version}.dmg"
+  else
+    sha256 'b3e7061206a1316477ecef8ec87be122f4f54e3942d6a659a5bb202a33a276f8'
+    url "https://github.com/acristoffers/Lachesis/releases/download/v#{version}/Lachesis-#{version}-arm64.dmg"
+  end
+
   appcast 'https://github.com/acristoffers/lachesis/releases.atom'
   name 'Lachesis'
   homepage 'https://github.com/acristoffers/Lachesis'
 
-  auto_updates true
+  auto_updates false
 
   app 'Lachesis.app'
 
   zap trash: [
                '~/Library/Application\ Support/lachesis',
                '~/Library/Preferences/me.acristoffers.lachesis.plist',
-               '~/Library/Logs/Lachesis',
+               '~/Library/Logs/Lachesis'
              ]
 end
