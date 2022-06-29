07e6f0479cf6ece3136d5c43a8f934b5a58b9220
Álan Crístoffer <acristoffers@startmail.com>
2022-06-29 08:33:06 +0200

    Fixes Lachesis's SHA256 on arm64.

--- Casks/lachesis.rb
+++ Casks/lachesis.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 cask 'lachesis' do
   version '1.3.6'
 
@@ -5,7 +7,7 @@ cask 'lachesis' do
     sha256 '37742668407f540ca21d4e3a22965e22b45e0cfecf2c12a5c2a38042e629308e'
     url "https://github.com/acristoffers/Lachesis/releases/download/v#{version}/Lachesis-#{version}.dmg"
   else
-    sha256 'b984af0e847bbd2a530511df1498eb6746ef70ce9e661f314d624e9758f13b57'
+    sha256 'd24683385e228ddf270dfae341c871e5bc10a9eb2a3963560602555aaeb684e7'
     url "https://github.com/acristoffers/Lachesis/releases/download/v#{version}/Lachesis-#{version}-arm64.dmg"
   end
 
@@ -18,8 +20,8 @@ cask 'lachesis' do
   app 'Lachesis.app'
 
   zap trash: [
-               '~/Library/Application\ Support/lachesis',
-               '~/Library/Preferences/me.acristoffers.lachesis.plist',
-               '~/Library/Logs/Lachesis'
-             ]
+    '~/Library/Application\ Support/lachesis',
+    '~/Library/Preferences/me.acristoffers.lachesis.plist',
+    '~/Library/Logs/Lachesis'
+  ]
 end
